8469355331cae09a333a7a40b6a70d9b4188959d
ajayAnthwal <ajayanthwal600@gmail.com>
2026-08-16 21:58:12 +0530

    update cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -3,12 +3,27 @@ import { env } from './env.config';
 
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    const allowedOrigins = env.CORS_ORIGIN === '*' ? [] : env.CORS_ORIGIN.split(',');
-    if (env.CORS_ORIGIN === '*' || !origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
+    // Allow non-browser requests (Postman, curl, server-to-server)
+    if (!origin) {
+      return callback(null, true);
     }
+
+    // If wildcard '*' is configured, dynamically echo back the requesting origin.
+    // This is MANDATORY for browsers when credentials: true is enabled!
+    if (env.CORS_ORIGIN === '*') {
+      return callback(null, origin);
+    }
+
+    const allowedOrigins = env.CORS_ORIGIN.split(',').map((o) => o.trim().replace(/\/$/, ''));
+    const normalizedOrigin = origin.replace(/\/$/, '');
+
+    if (allowedOrigins.includes(normalizedOrigin) || allowedOrigins.includes('*')) {
+      return callback(null, origin);
+    }
+
+    callback(new Error(`Origin ${origin} not allowed by CORS`));
   },
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
 };
